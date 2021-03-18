b46518b6624851417078bdc2596ed65aae0e7816
Edgar Martinez <ragde360@gmail.com>
2021-03-18 12:26:13 -0500

    Primer

---
+++ AutoWhats.Android/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Service.Notification;
+using Android;
+
+namespace AutoWhats.Droid
+{
+    [Activity(Label = "AutoWhats", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+#pragma warning disable CS0657 // No hay ninguna ubicación de atributo válida para esta declaración
+    [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+#pragma warning restore CS0657 // No hay ninguna ubicación de atributo válida para esta declaración
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Permission permiso_noty = CheckSelfPermission(Manifest.Permission.BindNotificationListenerService);
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(savedInstanceState);
+            Console.WriteLine("SUPERMAN:"+permiso_noty);
+
+
+
+            if (permiso_noty == Permission.Denied)
+            {
+                Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                StartActivity(intent);
+               
+            }
+
+/*
+           try {
+               
+                StartService(new Intent(this, typeof(NLService)));
+            } catch {
+               
+
+            }*/
+              
+            
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+
+        }
+
+
+
+
+        public override void OnBackPressed()
+        {
+            StopService(new Intent(this, typeof(NLService)));
+            base.OnBackPressed();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+
+          
+
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+       
+    }
+}
---
+++ AutoWhats.Android/NLService.cs
@@ -0,0 +1,97 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Service.Notification;
+using Android.Support.V4.App;
+using Android.Util;
+using Java.Lang;
+using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Object = Java.Lang.Object;
+
+namespace AutoWhats.Droid
+{
+    [Service(Label = "NLService", Permission = "android.permission.BIND_NOTIFICATION_LISTENER_SERVICE")]
+    [IntentFilter(new[] { "android.service.notification.NotificationListenerService" })]
+    public class NLService : NotificationListenerService
+    {
+        public override void OnCreate()
+        {   
+            base.OnCreate();
+            Log.Info("start running", "Servico Criado");
+        }
+        public override void OnDestroy()
+        {
+        
+            base.OnDestroy();
+        }
+        public override IBinder OnBind(Intent intent)
+        {
+         
+            return base.OnBind(intent);
+        }
+        public override bool OnUnbind(Intent intent)
+        {
+            StopService(new Intent(this, typeof(NLService)));
+            return base.OnUnbind(intent);
+        }
+        public override void OnNotificationPosted(StatusBarNotification sbn)
+        {
+            string packageName = sbn.PackageName;
+            try
+            {
+                Notification noty = sbn.Notification;
+                if (noty.Category.ToString() == "msg") {
+                    Bundle contenedr = noty.Extras;
+
+                    string demo = contenedr.GetCharSequence(NotificationCompat.ExtraTextLines);
+
+
+                    string[] multiMensajes = contenedr.GetCharSequenceArray("android.textLines");
+
+                    string content = noty.TickerText.ToString();
+                //    TextToSpeech.SpeakAsync("Mensaje" );
+                  //  TextToSpeech.SpeakAsync("de" );
+                    TextToSpeech.SpeakAsync(content);
+
+
+                    if (multiMensajes == null)
+                    {
+                        string mensaje = contenedr.GetString(NotificationCompat.ExtraText);
+                    
+                        TextToSpeech.SpeakAsync(mensaje);
+                    }
+                    else {
+
+                        foreach (string mensaje in multiMensajes) {
+                            TextToSpeech.SpeakAsync(mensaje);
+                        }
+                    }
+
+
+
+                    Console.WriteLine("YO SOY BATMAN");
+
+
+                    
+                }
+
+
+                base.OnNotificationPosted(sbn);
+            }
+            catch(System.Exception ex) {
+                Console.WriteLine("BATMAN DICE:"+ex);
+            }
+         
+
+        }
+
+        public override void OnNotificationRemoved(StatusBarNotification sbn)
+        {
+            base.OnNotificationRemoved(sbn);
+        }
+    }
+}
